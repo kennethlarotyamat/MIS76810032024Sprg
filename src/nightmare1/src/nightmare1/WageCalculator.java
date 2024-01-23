80ad1e81ca36b7af290c8dd5a72f58f877debd39
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-01-23 01:49:42 -0800

    1

--- src/nightmare1/src/nightmare1/WageCalculator.java
+++ src/nightmare1/src/nightmare1/WageCalculator.java
@@ -5,10 +5,17 @@ public class WageCalculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// constants
-		// how do I turn on autosave? the google instructions aren't making sense to me.		
+		// does eclipse ide autosace how do I turn on autosave? the google instructions aren't making sense to me.		
 
-		final double Base_PAY = 25;
+		final double BASE_PAY = 25;
 		final double REGULAR_HOURS = 40;
+		
+//		declare variables
+		double salary;  // salary to be calculated
+		double hours = 57; // hours worked
+		
+		salary = (hours - REGULAR_HOURS) * BASE_PAY * 0.5 + hours * BASE_PAY;
+		System.out.print("The salary is $" + salary);
 	}
 
 }
