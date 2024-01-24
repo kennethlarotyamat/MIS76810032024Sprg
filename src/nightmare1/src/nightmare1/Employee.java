f4e9a44b7c25763bc119f7284c2586837c47de7d
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-01-24 14:33:57 -0800

    1

--- src/nightmare1/src/nightmare1/Employee.java
+++ src/nightmare1/src/nightmare1/Employee.java
@@ -1,9 +1,42 @@
 package nightmare1;
 
 public class Employee {
+	private String fullName;
+	private double payRate;
+	
+	//	In Java, the keyword
+	//	 double
+	//	 is used to declare
+	//	 a variable that can store a double-precision floating-point
+	//	 number. It is a 64-bit
+	//	 data type
+	//	 that can store a wide range of decimal
+	//	 numbers, from 1.7e−308 to 1.7e+308.
+	
+	public Employee() {
+		fullName = "";
+		payRate = 13.5;
+	}
+	
+	public String getFullName() {
+		return fullName;
+	}
+	
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//The void keyword
+		// in Java is a reserved type used to specify that a method does not return
+		// any data type. A method that is declared as void does not have a return type
+		//, and it does not return a value when it is called.
+		//For example, the following method prints "Hello, World!" to the console:
+		//public void printMessage() {
+		//  System.out.println("Hello, World!");
+		// I had to cut my nails right now because i was finding it hard to type or maybe not hard but awkward		
+		}
 
 	}
 
