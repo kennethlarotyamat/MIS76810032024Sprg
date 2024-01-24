b964f08d432f02515367019fc1a3538c2e7f485f
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-01-24 15:03:26 -0800

    1

---
+++ src/nightmare1/src/nightmare1/Simple.java
@@ -0,0 +1,13 @@
+package nightmare1;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	double salary = 0;
+	System.out.println ("Programming is great fun");
+	
+
+}
