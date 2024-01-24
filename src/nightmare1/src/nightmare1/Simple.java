6ee9d29c8ad9d0cdaf89c79136bd7d630493785b
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-01-24 15:14:21 -0800

    1

--- src/nightmare1/src/nightmare1/Simple.java
+++ src/nightmare1/src/nightmare1/Simple.java
@@ -7,6 +7,14 @@ public class Simple {
 		
 		double salary = 0;
 		System.out.println ("Programming is great fun");
+		
+		Employee someone = new Employee();
+		
+		someone.setFullName("Gordisimo Elephante");
+		//		pronounced gor dee see mo el ay fawn tay
+		salary = someone.calDailyPay(8);
+		System.out.print(someone.getFullName() + "'s daily salary is $" + salary);
+		
 
 	}
 
