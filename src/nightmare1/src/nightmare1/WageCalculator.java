bcba8328b574e723ae1693134ddbfc435b22cfa5
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-01-24 02:22:22 -0800

    1

--- src/nightmare1/src/nightmare1/WageCalculator.java
+++ src/nightmare1/src/nightmare1/WageCalculator.java
@@ -23,5 +23,10 @@ public class WageCalculator {
 		// I'm already tired. I think that i have. i . well. i actually do have a group meeting
 		// for my other class later today.
 	}
+		// i had a small victory just now. i was able to get my info@chestnutandhazel email to link
+		// with my main github account but basically. it was kind of a difficult process.
+		// with a lot of things, there seems to be a kind of runaround, but i was eventually
+		// able to unlink info@chestnutandhazel from my old account, and re-link it after being unlinked
+		// to my main account. i think the old account was finally deleted. 
 
 }
