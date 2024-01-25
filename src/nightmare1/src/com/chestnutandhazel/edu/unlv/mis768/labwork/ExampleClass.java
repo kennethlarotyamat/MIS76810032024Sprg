64f5b734fb6ed8388f3a5333f69fb9e8dbe781dd
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-01-24 17:29:57 -0800

    1

---
+++ src/nightmare1/src/com/chestnutandhazel/edu/unlv/mis768/labwork/ExampleClass.java
@@ -0,0 +1,17 @@
+package com.chestnutandhazel.edu.unlv.mis768.labwork;
+
+public class ExampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int month, days;
+		
+		month = 2;
+		days = 28;
+		
+		System.out.println("Month"+ " " + month + " " + "has" + " " + days + " " + "days.");
+		System.out.println("Month "+month+" has "+days+" days.");
+
+	}
+
+}
