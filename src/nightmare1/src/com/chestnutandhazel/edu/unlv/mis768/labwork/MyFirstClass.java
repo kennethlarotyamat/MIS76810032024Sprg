abc6e805aa5b445aa7d83b1a5dc47ad3b702a96f
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-01-24 17:09:57 -0800

    1

--- src/nightmare1/src/com/chestnutandhazel/edu/unlv/mis768/labwork/MyFirstClass.java
+++ src/nightmare1/src/com/chestnutandhazel/edu/unlv/mis768/labwork/MyFirstClass.java
@@ -3,8 +3,8 @@ package com.chestnutandhazel.edu.unlv.mis768.labwork;
 public class MyFirstClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// print a message TODO Auto-generated method stub
+		System.out.print("Geetings Earth! This is Begoniatelli!");
 	}
 
 }
