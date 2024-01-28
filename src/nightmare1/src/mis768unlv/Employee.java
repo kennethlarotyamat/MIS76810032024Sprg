512889e80fef80d5c8de3ee6f02994d08b1c0d40
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-01-28 04:04:25 -0800

    1

--- src/nightmare1/src/mis768unlv/Employee.java
+++ src/nightmare1/src/mis768unlv/Employee.java
@@ -172,6 +172,54 @@ Subsection 4.3 Group
 
 
 
+Module 3: It-Enabled Innovation 
+
+3.1: The Relation Between It And Organizational Innovation 
+1: The Role Of It In Organizational Innovation 
+2: The Nonlinear Influence Of Harmonious Information Technology 
+3: Strategic Relevance Of Organizational Virtues Enabled By Information Technology 
+4: Information Technology And Organizational Innovation: 
+
+3.2: Disruptive Innovations With It
+1: Disruptive Innovation: Conceptual Foundations And Research Opportunities 
+2: Agility In Responding To Disruptive Digital Innovation
+3: The Disruptive Information Technology Innovation Model
+4: The Disruptive Nature Of Information Technology Innovations
+
+3.3: Innovating With Artificial Intelligence 
+1: Artificial Intelligence In Organizations 
+2: Ai Innovation Typology
+3: Artificial Intelligence (Ai): Multidisciplinary Perspectives 
+4: Strategic Use Of Ai
+
+References
+
+
+
+
+
+
+
+
+Annotations
+
+I Need To Find Out If It’s A Standard Convention To Truncate Titles Of Academic Articles.
+
+
+
+
+
+Module 1 Vicky
+Module 2 Kelly
+Module 3 Kenneth
+Module 4 Group
+Subsection 4.3 Group
+4.3.1 Digital Innovation Review Vicky
+4.3.2 Unpacking The Difference Kelly
+4.3.3 Digital Opportunities For Incumbents Kenneth
+4.3.4 Digitalization Ethical Challenge Kenneth
+
+
 
 
 */
