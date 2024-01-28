02ecf8b4c1ecff3cf158e68edaf111aa49575aad
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-01-28 04:11:25 -0800

    1

--- src/nightmare1/src/mis768unlv/Employee.java
+++ src/nightmare1/src/mis768unlv/Employee.java
@@ -219,6 +219,71 @@ Subsection 4.3 Group
 4.3.3 Digital Opportunities For Incumbents Kenneth
 4.3.4 Digitalization Ethical Challenge Kenneth
 
+I Need To Find Out If It’s A Standard Convention To Truncate Titles Of Academic Articles.
+
+
+Module 3: It-Enabled Innovation 
+
+3.1: The Relation Between It And Organizational Innovation 
+1: The Role Of It In Organizational Innovation 
+2: The Nonlinear Influence Of Harmonious Information Technology 
+3: Strategic Relevance Of Organizational Virtues Enabled By Information Technology 
+4: Information Technology And Organizational Innovation: 
+
+3.2: Disruptive Innovations With It
+1: Disruptive Innovation: Conceptual Foundations And Research Opportunities 
+2: Agility In Responding To Disruptive Digital Innovation
+3: The Disruptive Information Technology Innovation Model
+4: The Disruptive Nature Of Information Technology Innovations
+
+3.3: Innovating With Artificial Intelligence 
+1: Artificial Intelligence In Organizations 
+2: Ai Innovation Typology
+3: Artificial Intelligence (Ai): Multidisciplinary Perspectives 
+4: Strategic Use Of Ai
+
+References
+Mamonov, S., & Peterson, R. (2021). The role of IT in organizational innovation – A systematic literature review. Journal of Strategic Information Systems, 30, 101696. https://doi.org/10.1016/j.jsis.2021.101696
+
+Chatterjee, S., Moody, G. D., Lowry, P. B., Chakraborty, S., & Hardin, A. (2021). The nonlinear influence of harmonious information technology affordance on organisational innovation. Information Systems Journal, 31(3), 294–322. https://doi.org/10.1111/isj.12311
+
+Chatterjee, S., Moody, G., Lowry, P. B., Chakraborty, S., & Hardin, A. (2015). Strategic relevance of organizational virtues enabled by information technology in organizational innovation. Journal of Management Information Systems, 32(3), 158–196. https://doi.org/10.1080/07421222.2015.1099180
+
+Chatterjee, S., Moody, G., Lowry, P. B., Chakraborty, S., & Hardin, A. (2020). Information technology and organizational innovation: Harmonious information technology affordance and courage-based actualization. Journal of Strategic Information Systems, 29, 101596. https://doi.org/10.1016/j.jsis.2020.101596
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Annotations
+
+
+
+
+
+
+
+Module 1 Vicky
+Module 2 Kelly
+Module 3 Kenneth
+Module 4 Group
+Subsection 4.3 Group
+4.3.1 Digital Innovation Review Vicky
+4.3.2 Unpacking The Difference Kelly
+4.3.3 Digital Opportunities For Incumbents Kenneth
+4.3.4 Digitalization Ethical Challenge Kenneth
+
+
 
 
 
