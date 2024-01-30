6b9c39fb11faa14015b8ad1f6981e51b55279786
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-01-29 20:45:04 -0800

    1

---
+++ src/nightmare1/src/edu/unlv/mis768/week2/TicketPriceHFHIfElse.java
@@ -0,0 +1,16 @@
+package edu.unlv.mis768.week2;
+
+public class TicketPriceHFHIfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		final String CLASS_1 = "$15";
+		final String CLASS_2 = "$15";
+		final String CLASS_2 = "$25";
+		final String CLASS_3 = "$35";
+		final String OTHER = "Invalid";
+
+	}
+
+}
