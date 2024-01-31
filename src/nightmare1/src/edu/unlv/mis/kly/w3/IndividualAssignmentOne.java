7438346826c4cdbf937623a1a1b56a60ccc76583
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-01-31 00:46:59 -0800

    1

--- src/nightmare1/src/edu/unlv/mis/kly/w3/IndividualAssignmentOne.java
+++ src/nightmare1/src/edu/unlv/mis/kly/w3/IndividualAssignmentOne.java
@@ -8,6 +8,34 @@ public class IndividualAssignmentOne {
 
 /*
 
+
+
+
+
+
+package com.chestnutandhazel.edu.unlv.mis768.labwork;
+
+public class WageCalculatorP {
+
+	public static void main(String[] args) {
+		// constants
+		
+		final double BASE_PAY = 25;
+		final double REGULAR_HOURS = 40;
+		
+		// declare variables
+		double salary; // salary to be calculated
+		double hours = 57;
+		
+		salary = (hours - REGULAR_HOURS) * BASE_PAY * 0.5 + hours * BASE_PAY;
+		
+		
+		System.out.print("The Salary is $" + salary);
+
+	}
+
+}
+
 Resources:
 
 https://www.geeksforgeeks.org/java-math-class/
