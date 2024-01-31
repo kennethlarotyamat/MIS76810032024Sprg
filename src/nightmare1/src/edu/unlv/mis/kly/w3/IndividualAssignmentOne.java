3023fee492c35db482aeb47bf8a09b7ab9b7c7e1
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-01-31 00:19:16 -0800

    1

--- src/nightmare1/src/edu/unlv/mis/kly/w3/IndividualAssignmentOne.java
+++ src/nightmare1/src/edu/unlv/mis/kly/w3/IndividualAssignmentOne.java
@@ -22,6 +22,36 @@ Han-fen
 
 This announcement is closed for comments
 
+...
+
+Purpose
+Declare variables and constants and use them in a program.
+Design a decision structure to evaluate different conditions and execute different instructions accordingly.
+Handle user input and show output/results properly.
+Requirement
+Please provide proper comments to document your code, including the following:
+Author’s name
+Purpose of the program
+In-line comments for the statements, including the variable/constant declaration and initialization.
+Please submit the .java file(s) that contains the program for solving the following question.
+Write a program that computes the income tax for an individual. 
+The program should ask the user to enter the amount of taxable income. If the user enters a negative number, the program should show an error message and end the program. 
+
+The program then uses the tax bracket (as shown below) to calculate the tax amount:
+    10% on taxable income from $0 to $11,000, plus
+    12% on taxable income over $11,001 to $44,725, plus
+    22% on taxable income over $44,726 to $95,375, plus
+    24% on taxable income over $95,376 to $182,100, plus
+    32% on taxable income over $182,101to $231,250, plus
+    35% on taxable income over $231,251 to $578,125, plus
+    37% on taxable income over $578,126 or more
+
+The program should display the total income and the total tax due.
+
+Note: The income and tax amount should be rounded up to the next whole number. You can use Math.ceil(number) to get the round-up number.
+
+...
+
 
 
 
