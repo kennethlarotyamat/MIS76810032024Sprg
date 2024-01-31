9be5cf63cb5f76cda6a17f2b210bcf43a9b00fd7
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-01-31 00:18:09 -0800

    1

--- src/nightmare1/src/edu/unlv/mis/kly/w3/IndividualAssignmentOne.java
+++ src/nightmare1/src/edu/unlv/mis/kly/w3/IndividualAssignmentOne.java
@@ -3,3 +3,26 @@ package edu.unlv.mis.kly.w3;
 public class IndividualAssignmentOne {
 
 }
+
+/*
+
+Correction: Assignment 1 tax bracket
+Han-Fen Hu (She/Her)
+All Sections
+No unread replies.No replies.
+Hi All,
+
+I would like to make a correct on Individual Assignment 1: the tax bracket for 35% should start with $231,251 (instead of $231,250).
+
+I have updated the assignment sheet and the assignment page. Sorry about the error!
+
+Once again, Individual Assignment 1 is due on February 4. Please feel free to reach out if you have any questions or need help with the assignment.
+
+Han-fen
+
+This announcement is closed for comments
+
+
+
+
+ */
