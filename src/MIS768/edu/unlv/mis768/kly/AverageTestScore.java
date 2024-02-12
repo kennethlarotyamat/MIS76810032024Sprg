a1bb5fe752cf6d881a1683e166b447eca56c0533
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-02-12 00:08:35 -0800

    1

--- src/MIS768/edu/unlv/mis768/kly/AverageTestScore.java
+++ src/MIS768/edu/unlv/mis768/kly/AverageTestScore.java
@@ -1,6 +1,6 @@
 package edu.unlv.mis768.kly;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class AverageTestScore {
 
@@ -8,11 +8,12 @@ public class AverageTestScore {
 		// TODO Auto-generated method stub
 
 	}
-
+	// kb.close();
 }
 
 /*
-
+    <!-- 1 D:\GitHub\MIS76810032024Sprg\media\text\readme.txt -->
+    <!-- C:\Users\KLYam\OneDrive\Documents\GitHub\MIS76810032024Sprg\media\text\readme.md -->
 
 
 Methods
