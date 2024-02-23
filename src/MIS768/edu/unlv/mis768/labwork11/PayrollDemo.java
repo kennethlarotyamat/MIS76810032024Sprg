be284338cfc108b04c7c9f7b916d97d900326f42
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-02-23 04:45:52 -0800

    1

--- src/MIS768/edu/unlv/mis768/labwork11/PayrollDemo.java
+++ src/MIS768/edu/unlv/mis768/labwork11/PayrollDemo.java
@@ -1,40 +1,42 @@
 package edu.unlv.mis768.labwork11;
+
 import java.util.Scanner;
+
 public class PayrollDemo {
 
 	private static String classification;
 	private static String staff;
 	private static String manager;
 
-
 	public static void main(String[] args) {
-	    boolean validInput = false;
-																	// TO DO Auto-generated method stub
-																//	    Exception in thread "main" java.lang.NullPointerException: Cannot invoke "String.contains(java.lang.CharSequence)" because "str" is null
-	    do  {
+		boolean validInput = false;
+		// TO DO Auto-generated method stub
+		// Exception in thread "main" java.lang.NullPointerException: Cannot invoke
+		// "String.contains(java.lang.CharSequence)" because "str" is null
+		do {
 			Scanner keyboard = new Scanner(System.in);
-		    System.out.print("Is the employee in question classified as Staff or Manager?");
-		    																// classification = keyboard.nextLine();
-		    
-		    String str = keyboard.nextLine().toLowerCase(); 
-		    
-																				    //String str = keyboard.nextLine().toLowerCase();
-																				    
-																				    
-																			//	    Is the employee in question classified as Staff or Manager staff
-//	    	at edu.unlv.mis768.labwork11.PayrollDemo.main(PayrollDemo.java:16)
-	    if (str.contains("staff") || str.charAt(0) == 's') {
-            classification = staff;
-            System.out.println("The employee has been noted to fall into the Staff Classification.");
-            validInput = true;
-        } else if (str.contains("manager") || str.charAt(0) == 'm') {
-            classification = manager;
-            System.out.println("The employee has been noted to fall into the Manager Classification.");
-            validInput = true;
-        } else {
-            System.out.println("Please indicate if the employee in question is classified as either Staff or Manager.");
-        }
-	    // break;
-	} while (!validInput);
+			System.out.print("Is the employee in question classified as Staff or Manager?");
+			// classification = keyboard.nextLine();
+
+			String str = keyboard.nextLine().toLowerCase();
+
+			// String str = keyboard.nextLine().toLowerCase();
+
+			// Is the employee in question classified as Staff or Manager staff
+			// at edu.unlv.mis768.labwork11.PayrollDemo.main(PayrollDemo.java:16)
+			if (str.contains("staff") || str.charAt(0) == 's') {
+				classification = staff;
+				System.out.println("The employee has been noted to fall into the Staff Classification.");
+				validInput = true;
+			} else if (str.contains("manager") || str.charAt(0) == 'm') {
+				classification = manager;
+				System.out.println("The employee has been noted to fall into the Manager Classification.");
+				validInput = true;
+			} else {
+				System.out.println(
+						"Please indicate if the employee in question is classified as either Staff or Manager.");
+			}
+			// break;
+		} while (!validInput);
 	}
 }
