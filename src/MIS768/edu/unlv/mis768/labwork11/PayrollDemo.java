9baa8fbae1a60dae9d29f204c9262fe2ba20c7f4
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-02-23 05:08:50 -0800

    1

--- src/MIS768/edu/unlv/mis768/labwork11/PayrollDemo.java
+++ src/MIS768/edu/unlv/mis768/labwork11/PayrollDemo.java
@@ -12,20 +12,15 @@ public class PayrollDemo {
 
 	public static void main(String[] args) {
 		boolean validInput = false;
-		// TO DO Auto-generated method stub
-		// Exception in thread "main" java.lang.NullPointerException: Cannot invoke
-		// "String.contains(java.lang.CharSequence)" because "str" is null
+
 		do {
 			Scanner keyboard = new Scanner(System.in);
 			System.out.print("Is the employee in question classified as Staff or Manager?");
-			// classification = keyboard.nextLine();
+
 
 			String str = keyboard.nextLine().toLowerCase();
 
-			// String str = keyboard.nextLine().toLowerCase();
 
-			// Is the employee in question classified as Staff or Manager staff
-			// at edu.unlv.mis768.labwork11.PayrollDemo.main(PayrollDemo.java:16)
 			if (str.contains("staff") || str.charAt(0) == 's') {
 				classification = staff;
 				System.out.println("The employee has been noted to fall into the Staff Classification.");
@@ -38,20 +33,32 @@ public class PayrollDemo {
 				System.out.println(
 						"Please indicate if the employee in question is classified as either Staff or Manager.");
 			}
-			// break;
+
 		} while (!validInput);
 		System.out.println("Please enter the number of hours worked by the employee.");
 		if (classification.equals(staff)) {
-			// System.out.println("Please enter the number of hours worked by the
-			// employee.");
-			// if classification = staff {
-		} else if (classification.equals(manager)) {
-			// System.out.println("Please enter the number of hours worked by the
-			// employee.");
 
-		} else { // System.out.println("Please indicate if the employee in question is classified
-					// as either Staff or Manager.");
+		} else if (classification.equals(manager)) {
+
+
+		} else {
 
 		}
 	}
 }
+// TO DO Auto-generated method stub
+// Exception in thread "main" java.lang.NullPointerException: Cannot invoke
+// "String.contains(java.lang.CharSequence)" because "str" is null
+// classification = keyboard.nextLine();
+// System.out.println("Please indicate if the employee in question is classified
+// as either Staff or Manager.");
+// System.out.println("Please enter the number of hours worked by the
+// employee.");
+// System.out.println("Please enter the number of hours worked by the
+// employee.");
+// if classification = staff {
+// break;
+// String str = keyboard.nextLine().toLowerCase();
+
+// Is the employee in question classified as Staff or Manager staff
+// at edu.unlv.mis768.labwork11.PayrollDemo.main(PayrollDemo.java:16)
