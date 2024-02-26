7df1e37390d93ebb7f68cc1bea6960ac4698b51c
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-02-26 15:45:40 -0800

    1

--- src/MIS768/edu/unlv/mis768/kly/individualassignment2/redo2/TestScoreDemo.java
+++ src/MIS768/edu/unlv/mis768/kly/individualassignment2/redo2/TestScoreDemo.java
@@ -1,4 +1,4 @@
-package edu.unlv.mis768.kly.individualassignment2.redo;
+package edu.unlv.mis768.kly.individualassignment2.redo2;
 
 import java.util.ArrayList;
 
@@ -17,16 +17,11 @@ public class TestScoreDemo {
 
 			enterQuestionQty = JOptionPane
 					.showInputDialog("Please enter the number of test questions on the exam # " + (i + 1));
-			int numQuestions = Integer.parseInt(enterQuestionQty);
-			
-			TestScore testScore = new TestScore(0);
-			testScore.setNumQuestions(numQuestions);
 
-			
 			enterQuestionMissedQty = JOptionPane
 					.showInputDialog("Please enter the number of missed questions on the exam # " + (i + 1));
 
-			TestScore examPrintout = new TestScore(enterQuestionQty);
+			TestScore examPrintout = new TestScore(enterQuestionQty, enterQuestionMissedQty);
 
 			Double calculateQuestionValue = examPrintout.getPointsEach();
 			Double calculateExamScore = examPrintout.getScore();
@@ -56,28 +51,6 @@ public class TestScoreDemo {
 
 	}
 }
-// int numQuestions = Integer.parseInt(enterQuestionQty);
-// I don't know what's going on, but it's not working any more.
-// , enterQuestionMissedQty
-//	private double questionValue; 	 // question value Removed // 1. theScore and questionValue should be removed. They are values determined by other fields. Whenever the two are needed, just call getScore() and getPointsEach()
-
-/*
-
-You have a very good starting point. I think you over-think the question.
-
-Please revise the program and resubmit. Please let me know if you need me to walk over this with you.
-
-
-A few issues of the program that can be fixed:
-1. theScore and questionValue should be removed. They are values determined by other fields. Whenever the two are needed, just call getScore() and getPointsEach()
-2. The equals() method needs to be revise accordingly. Rather than using theScore, use getScore().
-3. The getScore() should just return the calculation result, instead of assigning the value to theScore.
-4. The getPointsEach() method will just return the calculation result, instead of assignment the value to questionValue.
-5. setNumMissed() should check the value and assign the value to numMissed.
-6. According to the class diagram, the constructor should have only one parameter, numQ.
-Han-Fen Hu , Feb 25 at 3:20pm
-
-*/
 
 // this was added to remove the warning
 // 		ArrayList<Double> questionValueArray = new ArrayList<Double>();  // this was added to remove the warning
