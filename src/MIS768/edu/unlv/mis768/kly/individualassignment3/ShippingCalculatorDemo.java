1901b2a1679f8a899a35fa3925efc0a6ea2e3aed
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-02-29 02:29:25 -0800

    1

--- src/MIS768/edu/unlv/mis768/kly/individualassignment3/ShippingCalculatorDemo.java
+++ src/MIS768/edu/unlv/mis768/kly/individualassignment3/ShippingCalculatorDemo.java
@@ -8,10 +8,31 @@
 */
 package edu.unlv.mis768.kly.individualassignment3;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ShippingCalculatorDemo {
+	
+	public static void main(String[] args) throws IOException {
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Greetings: Utilize this terminal to determine the cost to ship your parcel. ");
+		System.out.println("           Please enter the weight and the dimensions of your parcel: ");
+		
+	}
 
 
 }
 
 // public class ShippingCalculatorDemo {
 // The name of a class should be a noun/noun-phrase.
+// I'm frustrated right now. everything is a disaster, and nothing is going right.'
+// and I don't anticipate things getting better. this is a nightmare.'
+
+
+/*
+"Dimensional weight, also known as volumetric weight, is a pricing technique used 
+by postal services and commercial freight companies. It's based on the cubic capacity 
+of a package, rather than its weight.""
+*/
